50134a0492de1a14e3d11154d59fbcc39d57d698
anirban <anirbanguharoy82@gmail.com>
2025-10-12 00:46:50 +0530

    minute changes in update and delete route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require("express")
 const app=express()
 app.use(express.json())
 
-const blogs=[]
+let blogs=[]
 
 app.post("/blogs",(req,res)=>{
   blogs.push({...req.body,id:blogs.length+1})
@@ -27,13 +27,33 @@ app.get("/blogs/:id",(req,res)=>{
 
 app.patch("/blogs",(req,res)=>{
 
+
 })
 
 app.patch("/blogs/:id",(req,res)=>{
 
+  const{id}=req.params
+
+  // let index=blogs.findIndex(blog=>blog.id==id)
+
+  // blogs[index]={...blogs[index],...req.body}
+
+  let updatedBlogs=blogs.map((blog,index)=>blog.id==id?({...blogs[index],...req.body}):blog)
+
+  blogs=[...updatedBlogs]
+
+  return res.json({message:"blog updated successfully",updatedBlogs})
+
 })
 
-app.delete("/blogs",(req,res)=>{
+app.delete("/blogs/:id",(req,res)=>{
+  const{id}=req.params
+
+   let deleteBlog=blogs.filter(blog=>blog.id!=id)
+
+   blogs=[...deleteBlog]
+
+   return res.json({message:"deleted blog successfully"})
 
 })
 
