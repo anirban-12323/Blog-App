4eb94666cd3838db12f66514408f62a473ce541d
anirban <anirbanguharoy82@gmail.com>
2025-10-12 17:15:40 +0530

    user routes added

--- server.js
+++ server.js
@@ -2,6 +2,148 @@ const express=require("express")
 const app=express()
 app.use(express.json())
 
+//USER ROUTES
+let users=[]
+app.post("/users",(req,res)=>{
+  const{name,email,password}=req.body
+  try {
+    
+    if(!name){
+      return res.status(400).json({
+        success:false,
+        message:"Please enter name"
+      })
+    }
+     if(!password){
+      return res.status(400).json({
+        success:false,
+        message:"Please enter password"
+      })
+    }
+     if(!email){
+      return res.status(400).json({
+        success:false,
+        message:"Please enter email"
+      })
+    }
+
+    users.push({...req.body,id:users.length+1})
+    return res.status(200).json({
+      success:true,
+      message:"User created successfully"
+    })
+    
+  } catch (error) {
+
+    return res.status(400).json({
+      success:false,
+      message:"Please try again"
+
+    })
+    
+  }
+
+})
+
+app.get("/users",(req,res)=>{
+  try {
+    
+    return res.status(200).json({
+      success:true,
+      message:"user fetch successfully",
+      users
+    })
+    
+  } catch (error) {
+     return res.status(400).json({
+      success:false,
+      message:"error occur during user fetching"
+
+    })
+    
+  }
+})
+
+
+app.get("/users/:id",(req,res)=>{
+
+  try {
+    const user=users.filter(user=>user.id==req.params.id)
+    if(user.length==0){
+      return res.status(400).json({
+      success:false,
+      message:"user not found",
+      user
+    })
+
+    }
+    return res.status(200).json({
+      success:true,
+      message:"user fetch successfully",
+      user
+    })
+    
+  } catch (error) {
+     return res.status(400).json({
+      success:false,
+      message:"error occur during user fetching"
+
+    })
+    
+  }
+
+})
+
+app.patch("/users/:id",(req,res)=>{
+  
+  try {
+    const{id}=req.params
+    let updatedUser=users.map((user,index)=>user.id==id?({...users[index],...req.body}):user)
+    users=[...updatedUser]
+    return res.status(200).json({
+      success:true,
+      message:"user updated successfully",
+      updatedUser})
+    
+  } catch (error) {
+    return res.status(400).json({
+      success:false,
+      message:"error during user update"
+
+    })
+    
+    
+  }
+})
+
+app.delete("/users/:id",(req,res)=>{
+ 
+
+   try {
+     const{id}=req.params
+     let deleteUser=users.filter(user=>user.id!=id)
+     users=[...deleteUser]
+     return res.status(200).json({
+      success:true,
+      message:"user deleted successfully",
+      users
+     })
+    
+   } catch (error) {
+    return res.status(400).json({
+      success:false,
+      message:"Faceing error during deleting the user"
+    })
+    
+   }
+})
+
+
+
+
+
+
+//BLOG ROUTES
 let blogs=[]
 
 app.post("/blogs",(req,res)=>{
@@ -25,10 +167,7 @@ app.get("/blogs/:id",(req,res)=>{
 
 })
 
-app.patch("/blogs",(req,res)=>{
 
-
-})
 
 app.patch("/blogs/:id",(req,res)=>{
 
